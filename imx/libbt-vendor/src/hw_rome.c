121d3222c1217df5681b0066da35fd3271488c43
Wig Cheng <wig.cheng@technexion.com>
2019-07-09 11:19:50 +0800

    hciattach_rome: fix NVM file ignore loading issue, upgrade to bluetooth 5
    It will be fixed on bluetooth 4.1 if no fix this issue, but it also need to
    avoid overriding MAC address.
    
    Original patch is refer from Technexion Linux source from github:
    
    From 49171197698886eb815656c3c48a98e014453781 Mon Sep 17 00:00:00 2001
    From: Ray Chang <ray.chang@technexion.com>
    Date: Thu, 20 Jun 2019 14:17:38 +0800
    Subject: [PATCH 1/2] hciattach_rome: do not override module MAC address

--- imx/libbt-vendor/src/hw_rome.c
+++ imx/libbt-vendor/src/hw_rome.c
@@ -70,6 +70,7 @@ unsigned char *phdr_buffer;
 unsigned char *pdata_buffer = NULL;
 patch_info rampatch_patch_info;
 int rome_ver = ROME_VER_UNKNOWN;
+unsigned char bd_addr[BD_ADDR_LEN];
 unsigned char gTlv_type;
 unsigned char gTlv_dwndCfg;
 static unsigned int wipower_flag = 0;
@@ -222,6 +223,9 @@ int get_vs_hci_event(unsigned char *rsp)
 
 		case NVM_ACCESS_CODE:
 			ALOGI("%s: NVM Access Code!!!", __FUNCTION__);
+			if((rsp[TAG_NUM_OFFSET] == TAG_NUM_2)&&(rsp[6] == BD_ADDR_LEN)) {
+				memcpy(bd_addr, &rsp[TAG_BDADDR_OFFSET], BD_ADDR_LEN);
+			}
 			err = HCI_CMD_SUCCESS;
 			break;
 		case EDL_SET_BAUDRATE_RSP_EVT:
@@ -380,6 +384,47 @@ int hci_send_vs_cmd(int fd, unsigned char *cmd, unsigned char *rsp, int size)
 
 failed:
 	return ret;
+}
+
+int rome_hci_bd_address(int fd)
+{
+	int size, err = 0;
+	unsigned char cmd[HCI_MAX_CMD_SIZE];
+	unsigned char rsp[HCI_MAX_EVENT_SIZE];
+	hci_command_hdr *cmd_hdr;
+
+	fprintf(stderr, "%s: HCI READ BD ADDRESS \n", __FUNCTION__);
+
+	memset(cmd, 0x0, HCI_MAX_CMD_SIZE);
+
+	cmd_hdr = (void *) (cmd + 1);
+	cmd[0]  = HCI_COMMAND_PKT;
+	cmd_hdr->opcode = cmd_opcode_pack(HCI_VENDOR_CMD_OGF, HCI_PS_CMD_OCF);
+	cmd_hdr->plen   = 2;
+	cmd[4]  = 0;
+	cmd[5]  = 2;
+
+	/* Total length of the packet to be sent to the Controller */
+	size = (HCI_CMD_IND + HCI_COMMAND_HDR_SIZE + 2);
+
+	fprintf(stderr, "%s: HCI CMD: 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x\n", __FUNCTION__, cmd[0], cmd[1], cmd[2], cmd[3], cmd[4], cmd[5]);
+
+	/* Send HCI Command packet to Controller */
+	err = hci_send_vs_cmd(fd, (unsigned char *)cmd, rsp, size);
+	if ( err != size) {
+		fprintf(stderr, "Failed to set the patch info to the Controller!\n");
+		goto error;
+	}
+
+	err = read_hci_event(fd, rsp, HCI_MAX_EVENT_SIZE);
+	if ( err < 0) {
+		fprintf(stderr, "%s: Failed to set patch info on Controller\n", __FUNCTION__);
+		goto error;
+	}
+	fprintf(stderr, "%s: Successfully set patch info on the Controller\n", __FUNCTION__);
+
+error:
+	return err;
 }
 
 void frame_hci_cmd_pkt(unsigned char *cmd, int edl_cmd,
@@ -869,24 +914,14 @@ int rome_get_tlv_file(char *file_path, unsigned char baud_rate)
 
 			/* Write BD Address */
 			if (nvm_ptr->tag_id == TAG_NUM_2) {
-				if (vnd_local_bd_addr[0] != 0) {
+				if(vnd_local_bd_addr[0] == 0)
 					memcpy(nvm_byte_ptr, vnd_local_bd_addr, 6);
-					ALOGI("BD Address: %.02x:%.02x:%.02x:%.02x:%.02x:%.02x",
-					      *nvm_byte_ptr, *(nvm_byte_ptr+1), *(nvm_byte_ptr+2),
-					      *(nvm_byte_ptr+3), *(nvm_byte_ptr+4), *(nvm_byte_ptr+5));
-				} else {
-					/* Remove it from NVM data */
-					int nvm_size = nvm_ptr->tag_len + sizeof(tlv_nvm_hdr);
-					ALOGI("Skip BD Address from NVM\n");
-					nvm_index += nvm_ptr->tag_len;
-					nvm_byte_ptr += nvm_ptr->tag_len;
-					memmove(nvm_ptr, nvm_byte_ptr, nvm_length - nvm_index);
-					nvm_length -= nvm_size;
-					nvm_byte_ptr -= nvm_size;
-					readSize -= nvm_size;
-					nvm_index -= nvm_size;
-					continue;
-				}
+				else
+					memcpy(nvm_byte_ptr, vnd_local_bd_addr, 6);
+
+				ALOGI("BD Address: %.02x:%.02x:%.02x:%.02x:%.02x:%.02x",
+				*nvm_byte_ptr, *(nvm_byte_ptr+1), *(nvm_byte_ptr+2),
+				*(nvm_byte_ptr+3), *(nvm_byte_ptr+4), *(nvm_byte_ptr+5));
 			}
 
 			if (nvm_ptr->tag_id == TAG_NUM_17) {
@@ -1530,6 +1565,8 @@ int rome_soc_init(int fd, int speed)
 	}
 
 	ALOGD("%s: Rome Version (0x%08x)\n", __FUNCTION__, rome_ver);
+
+	rome_hci_bd_address(fd);
 
 	switch (rome_ver){
 		case ROME_VER_1_0:
